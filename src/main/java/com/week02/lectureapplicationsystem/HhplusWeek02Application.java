e0ce7380d6b2e5154b04576eaece95efa24c110f
박성현 <qkr970504@gmail.com>
2024-12-23 17:17:49 +0900

    [init] build project

---
+++ src/main/java/com/week02/lectureapplicationsystem/HhplusWeek02Application.java
@@ -0,0 +1,13 @@
+package com.week02.lectureapplicationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusWeek02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusWeek02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/week02/lectureapplicationsystem/HhplusWeek02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week02.lectureapplicationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusWeek02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
